9477e5d7db692bf7d3f6068fe1f9e30b5eda4d3c
srankmeng <jaturong.duang@gmail.com>
2023-02-08 12:57:29 +0700

    [feat] Created vpc, igw, subnet, sg, route table and connect rds with new sg

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
+  region  = var.region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
@@ -20,8 +20,107 @@ locals {
   db_creds = jsondecode(data.aws_secretsmanager_secret_version.terraform_db_current.secret_string)
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "VPC was created by Terraform"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  
+  tags = {
+    Name = "IGW was created by Terraform"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+resource "aws_subnet" "public_subnets" {
+  count      = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  
+  tags = {
+    Name = "Public Subnet ${count.index + 1} was created by Terraform"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  
+  tags = {
+    Name = "Route Table was created by Terraform"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+resource "aws_route_table_association" "public_subnet_asso" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.route_table.id
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "ec2" {
+  name = "terraform-sg"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    description = "Postgres"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    description = "HTTPS"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security group was created by Terraform"
+  }
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db-subnet-group"
+  subnet_ids = [for subnet in aws_subnet.public_subnets : subnet.id]
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance#argument-reference
-resource "aws_db_instance" "rds_instance" {
+resource "aws_db_instance" "rds" {
   identifier             = "rds-terraform"
   instance_class         = "db.t2.micro"
   allocated_storage      = 5
@@ -29,8 +128,8 @@ resource "aws_db_instance" "rds_instance" {
   engine_version         = "12.13"
   skip_final_snapshot    = true
   publicly_accessible    = true
-  # vpc_security_group_ids = [aws_security_group.uddin.id]
-  # security_groups
+  db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.id
+  vpc_security_group_ids = [aws_security_group.ec2.id]
   db_name                = local.db_creds.dbname
   username               = local.db_creds.username
   password               = local.db_creds.password
