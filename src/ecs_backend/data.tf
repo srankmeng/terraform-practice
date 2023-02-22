2c63d9becd2044dde2f4a8fbe69b0768974ebd8f
srankmeng <jaturong.duang@gmail.com>
2023-02-22 23:06:39 +0700

    [feat] Rename iam role ecsTaskExecutionRole

--- src/ecs_backend/data.tf
+++ src/ecs_backend/data.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 data "aws_iam_role" "ecsTaskExecutionRole" {
-  name = "ecsTaskExecutionRole"
+  name = "tf-ecsTaskExecutionRole"
 }
 
 data "aws_vpc" "vpc" {
