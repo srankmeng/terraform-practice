659f33d38f309d17502fc732f454dbbca3e7c96a
srankmeng <jaturong.duang@gmail.com>
2023-02-23 00:53:17 +0700

    [feat] Add s3 & cloudfront

---
+++ src/s3/main.tf
@@ -0,0 +1,104 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
+locals {
+  bucket_name = "tf-s3-website"
+}
+
+resource "aws_s3_bucket" "site" {
+  bucket = local.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "terraform"
+}
+data "aws_iam_policy_document" "origin_access_identity" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.site.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "site_policy" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.origin_access_identity.json
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  comment = "by terraform (tf-website)"
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id   = local.bucket_name
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.bucket_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  price_class = "PriceClass_200"
+
+  tags = {
+    Name        = "terraform s3 distribution"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
