f5154d42391d6b43b098ff5a72e64ae4695e6d23
srankmeng <jaturong.duang@gmail.com>
2023-02-28 16:41:13 +0700

    [feat] Apply cors on api gateway

--- src/api_gateway/main.tf
+++ src/api_gateway/main.tf
@@ -19,6 +19,9 @@ resource "aws_api_gateway_rest_api" "api_gw"{
   description = "terraform api gw"
 }
 
+
+##################
+### users
 resource "aws_api_gateway_resource" "resource_users" {
   rest_api_id = aws_api_gateway_rest_api.api_gw.id
   parent_id   = aws_api_gateway_rest_api.api_gw.root_resource_id
@@ -35,12 +38,22 @@ resource "aws_api_gateway_method" "method_users" {
   # }
 }
 
+resource "aws_api_gateway_method_response" "response_users" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gw.id
+  resource_id   = aws_api_gateway_resource.resource_users.id
+  http_method = aws_api_gateway_method.method_users.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
 resource "aws_api_gateway_integration" "integration_users" {
   rest_api_id = aws_api_gateway_rest_api.api_gw.id
   resource_id = aws_api_gateway_resource.resource_users.id
   http_method = aws_api_gateway_method.method_users.http_method
   integration_http_method = "GET"
-  type                    = "HTTP_PROXY"
+  type                    = "HTTP"
   uri                     = "http://${data.aws_lb.backend_users_alb.dns_name}/users"
  
   # request_parameters =  {
@@ -48,6 +61,73 @@ resource "aws_api_gateway_integration" "integration_users" {
   # }
 }
 
+resource "aws_api_gateway_integration_response" "users" {
+  rest_api_id = aws_api_gateway_rest_api.api_gw.id
+  resource_id = aws_api_gateway_resource.resource_users.id
+  http_method = aws_api_gateway_method.method_users.http_method
+  status_code = aws_api_gateway_method_response.response_users.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "${var.allow_origin}"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.integration_users
+  ]
+}
+
+##################
+### users (options)
+resource "aws_api_gateway_method" "method_users_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gw.id
+  resource_id   = aws_api_gateway_resource.resource_users.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "response_users_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gw.id
+  resource_id   = aws_api_gateway_resource.resource_users.id
+  http_method = aws_api_gateway_method.method_users_options.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "integration_users_options" {
+  rest_api_id = aws_api_gateway_rest_api.api_gw.id
+  resource_id = aws_api_gateway_resource.resource_users.id
+  http_method = aws_api_gateway_method.method_users_options.http_method
+  type = "MOCK"
+  passthrough_behavior = "WHEN_NO_MATCH"
+  request_templates = {
+    "application/json" : "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.api_gw.id
+  resource_id = aws_api_gateway_resource.resource_users.id
+  http_method = aws_api_gateway_method.method_users_options.http_method
+  status_code = aws_api_gateway_method_response.response_users_options.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "${var.allow_headers}"
+    "method.response.header.Access-Control-Allow-Methods" = "${var.allow_methods}"
+    "method.response.header.Access-Control-Allow-Origin"  = "${var.allow_origin}"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.integration_users_options
+  ]
+}
+
+##################
+### products
 resource "aws_api_gateway_resource" "resource_products" {
   rest_api_id = aws_api_gateway_rest_api.api_gw.id
   parent_id   = aws_api_gateway_rest_api.api_gw.root_resource_id
@@ -100,7 +180,13 @@ resource "aws_api_gateway_deployment" "ApiDeploymentDev" {
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.resource_users,
       aws_api_gateway_method.method_users,
+      aws_api_gateway_method_response.response_users,
       aws_api_gateway_integration.integration_users,
+      aws_api_gateway_integration_response.users,
+      aws_api_gateway_method.method_users_options,
+      aws_api_gateway_method_response.response_users_options,
+      aws_api_gateway_integration.integration_users_options,
+      aws_api_gateway_integration_response.options,
       aws_api_gateway_resource.resource_products,
       aws_api_gateway_method.method_products,
       aws_api_gateway_integration.integration_products,
--- src/api_gateway/var.tf
+++ src/api_gateway/var.tf
@@ -7,3 +7,18 @@ variable "region" {
   type        = string
   default     = "ap-southeast-1"
 }
+
+variable "allow_headers" {
+  type        = string
+  default     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+}
+
+variable "allow_methods" {
+  type        = string
+  default     = "'GET,POST,PUT,OPTIONS'"
+}
+
+variable "allow_origin" {
+  type        = string
+  default     = "'https://d3gtphjcnn0s2m.cloudfront.net'"
+}
